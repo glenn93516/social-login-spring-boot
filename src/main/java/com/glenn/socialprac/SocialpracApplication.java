aeffee565b7e81840d51406834cf3494b2cc8aef
김민혁 <glenn93516@gmail.com>
2021-04-18 18:48:48 +0900

    Init project

---
+++ src/main/java/com/glenn/socialprac/SocialpracApplication.java
@@ -0,0 +1,13 @@
+package com.glenn.socialprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialpracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glenn/socialprac/SocialpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glenn.socialprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
